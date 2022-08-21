0ea624ebf39de91674e041b3e83bc8462b95f7fe
fan_1998 <2680811484@qq.com>
2022-08-21 20:47:54 +0800

    相似度对比

--- src/main/java/com/utils/SimilarUtils.java
+++ src/main/java/com/utils/SimilarUtils.java
@@ -10,45 +10,55 @@ public class SimilarUtils {
 
 
     public static void main(String[] args) {
-        double x =  getStringSimilarity("b>0 && a>0","b>0 && a>0 c==0");
+        float x =  getSimilarityRatio("b>0 && a>0","b>0 && a>0 c==0");
         System.out.println(x);
     }
 
 
-    public static double getStringSimilarity(String sourceString, String targetString) {
-        int[][] matrix;
-        int sourceStringLength = sourceString.length();
-        int targetStringLength = targetString.length();
-        int indexOfSourceString;
-        int indexOfTargetString;
-        char charOfSourceString;
-        char charOfTargetString;
-        int temp;
+    /**
+     * 比较两个字符串的相识度
+     * 核心算法：用一个二维数组记录每个字符串是否相同，如果相同记为0，不相同记为1，每行每列相同个数累加
+     * 则数组最后一个数为不相同的总数，从而判断这两个字符的相识度
+     * @param str
+     * @param target
+     * @return
+     */
+    public static float getSimilarityRatio(String str, String target) {
 
-        if (sourceStringLength == 0 || targetStringLength == 0) return 0;
-
-        matrix = new int[sourceStringLength + 1][targetStringLength + 1];
-        for (indexOfSourceString = 0; indexOfSourceString <= sourceStringLength; indexOfSourceString++) {
-            matrix[indexOfSourceString][0] = indexOfSourceString;
+        int d[][]; // 矩阵
+        int n = str.length();
+        int m = target.length();
+        int i; // 遍历str的
+        int j; // 遍历target的
+        char ch1; // str的
+        char ch2; // target的
+        int temp; // 记录相同字符,在某个矩阵位置值的增量,不是0就是1
+        if (n == 0 || m == 0) {
+            return 100.0F;
         }
-        for (indexOfTargetString = 0; indexOfTargetString <= targetStringLength; indexOfTargetString++) {
-            matrix[0][indexOfTargetString] = indexOfTargetString;
+        d = new int[n + 1][m + 1];
+        for (i = 0; i <= n; i++) { // 初始化第一列
+            d[i][0] = i;
         }
 
-        for (indexOfSourceString = 1; indexOfSourceString <= sourceStringLength; indexOfSourceString++) {
-            charOfSourceString = sourceString.charAt(indexOfSourceString - 1);
-            for (indexOfTargetString = 1; indexOfTargetString <= targetStringLength; indexOfTargetString++) {
-                charOfTargetString = targetString.charAt(indexOfTargetString - 1);
-                if (charOfSourceString == charOfTargetString || charOfSourceString == charOfTargetString + 32 || charOfSourceString + 32 == charOfTargetString) {
+        for (j = 0; j <= m; j++) { // 初始化第一行
+            d[0][j] = j;
+        }
+
+        for (i = 1; i <= n; i++) { // 遍历str
+            ch1 = str.charAt(i - 1);
+            // 去匹配target
+            for (j = 1; j <= m; j++) {
+                ch2 = target.charAt(j - 1);
+                if (ch1 == ch2 || ch1 == ch2 + 32 || ch1 + 32 == ch2) {
                     temp = 0;
-                } else temp = 1;
-                matrix[indexOfSourceString][indexOfTargetString] =
-                        Math.min(Math.min(matrix[indexOfSourceString - 1][indexOfTargetString] + 1,
-                                matrix[indexOfSourceString][indexOfTargetString - 1] + 1),
-                                matrix[indexOfSourceString - 1][indexOfTargetString - 1] + temp);
+                } else {
+                    temp = 1;
+                }
+                // 左边+1,上边+1, 左上角+temp取最小
+                d[i][j] = Math.min(Math.min(d[i - 1][j] + 1, d[i][j - 1] + 1), d[i - 1][j - 1] + temp);
             }
         }
-        return (1 - (double) matrix[sourceStringLength][targetStringLength] / Math.max(sourceString.length(), targetString.length())) * 100F;
+        return (1 - (float) d[n][m] / Math.max(str.length(), target.length())) * 100F;
     }
-
 }
